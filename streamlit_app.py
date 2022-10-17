d6e1e7b3e4d8daa62fd7531617d1f5d0b0ed78c0
Aditya Khetan <115969021+agkhetan1988@users.noreply.github.com>
2022-10-17 11:32:36 +0530

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 #Import libraries
 import streamlit
+import pandas
 
 #App Title
 streamlit.title('My Parents New Healthy Diner')
@@ -14,10 +15,8 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 streamlit.text('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 #My Fruit List Dataframe
-#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#streamlit.dataframe(my_fruit_list)
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 #Let's put a pick list here so they can pick the fruits they want to include
 streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
