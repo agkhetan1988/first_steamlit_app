ffc0278fdc40b056297ff8545f3ad6112031c2f0
Aditya Khetan <115969021+agkhetan1988@users.noreply.github.com>
2022-10-19 23:20:30 +0530

    Update streamlit_app.py
    Removed the json displayed unnormalized

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,6 @@ streamlit.header('Fruityvice Fruity Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 #take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
